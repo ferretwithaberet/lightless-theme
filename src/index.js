4af45d3889c59da0bad352b5f467996e9c3942d3
Andrei Radoi <andreiradoidev@gmail.com>
2020-06-08 19:31:44 +0300

    Changes to colors, improvements to build script

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 const fs = require('fs');
-const theme = require('./schemes/purple.json');
+const Color = require('color');
 
+// Colors
+const primary = Color(theme['primary']);
+const black = Color('#000000');
+const white = Color('#ffffff');
 
+// Theme
+let theme = require('./schemes/purple.json');
+
+theme = {
+    ...theme,
+    ...{
+        // Basic colors
+        "black": black.hex(),
+        "white": white.hex(),
+        "grey": white.darken(.47).hex(),
+        "transparent": Color('#00000000').hex(),
+        // Base backgrounds
+        "primaryBackground": white.darken(.98).hex(),
+        "secondaryBackground": white.darken(.93).hex(),
+        // Shades of primary
+        "primaryLighter": primary.lighten(.25).hex(),
+        "primaryHover": primary.darken(.15).hex(),
+        "primaryDarker": primary.darken(.25).hex(),
+        "primary-1": primary.hex() + Math.round(.25 * 255).toString(16),
+        "primary-2": primary.hex() + Math.round(.5 * 255).toString(16),
+        "primary-3": primary.hex() + Math.round(.75 * 255).toString(16),
+        // Shades of transparent
+        "darken-1": black.hex() + Math.round(.25 * 255).toString(16),
+        "darken-2": black.hex() + Math.round(.5 * 255).toString(16),
+        "darken-3": black.hex() + Math.round(.75 * 255).toString(16),
+        "lighten-1": white.hex() + Math.round(.25 * 255).toString(16),
+        "lighten-2": white.hex() + Math.round(.5 * 255).toString(16),
+        "lighten-3": white.hex() + Math.round(.75 * 255).toString(16)
+    }
+}
+
+// Create theme file
 fs.readFile('./src/themes/lightless-dark.json', 'utf8', (err, data) => {
     if (err) throw err;
     // For each key in theme, replace it in the theme data
